57e8200e263ff74431224e223693c31cbee65d5d
Jaymin Gajjar <jaymin.gajjar@esprit.co.in>
2017-06-17 20:36:25 +0530

    Wait for user input and if any then override wordpress call

--- morseCode/blink.py
+++ morseCode/blink.py
@@ -5,6 +5,7 @@ from wordpress_xmlrpc.methods import options
 import time
 import traceback
 import requests
+import threading
 
 
 MORSECODE = {' ': ' ',
@@ -86,7 +87,7 @@ def callApi():
         global speed
         global pinNum
         global timeInterval
-        # wp = Client('http://192.168.1.42/emp/dipesha/wordpress/xmlrpc.php', 'user', 'pass')
+        # wp = Client('http://192.168.1.42/rpie/wordpress/xmlrpc.php', 'user', 'pass')
         # display_text = wp.call(options.GetOptions('python_button_clicked'))
         # pinNum = wp.call(options.GetOptions('pinNum'))
         # speed = wp.call(options.GetOptions('speed'))
@@ -97,17 +98,41 @@ def callApi():
         pinNum = data["output_pin"]
         timeInterval = data["time"]
         speed = data["speed"]
+        print ("")
         print ("API call successful to wordpress. New message to display is : ")
         print (display_text)
         print ("pinNum : ",pinNum)
         print ("speed :",speed)
         print ("timeInterval :",timeInterval)
+        print ("----------------------------------------------------")
         return display_text
+
+# Declare a mutable object so that it can be pass via reference
+user_input = [None]
+
+# spawn a new thread to wait for input 
+def get_user_input(user_input_ref):
+        while True:
+                user_input_ref[0] = input("Override Message: ")
+
+inputThread = threading.Thread(target=get_user_input, args=(user_input,))
+inputThread.daemon = True
+inputThread.start()
+
+displaying_terminal_message = False
 
 while True:
         # outputString = input('Enter your text : ')
         #outputString = 'Hello World';
-        outputString = callApi();
+        # print (user_input)
+        if user_input[0] is not None:
+                outputString = user_input[0];
+                displaying_terminal_message = True
+                print ("Message overridden by terminal input : ",outputString)
+                print ("----------------------------------------------------")
+        else:
+                displaying_terminal_message = False
+                outputString = callApi();
 
         for letter in outputString:
                         for symbol in MORSECODE[letter.upper()]:
@@ -118,8 +143,15 @@ while True:
                                 else:
                                         time.sleep(0.5*speed)
                         time.sleep(0.5*speed)
-        print ("Now waiting for ",timeInterval," seconds for another call")
-        time.sleep(timeInterval)
+        
+        if user_input[0] is None:
+                print ("Now waiting for ",timeInterval," seconds for another call")
+                time.sleep(timeInterval)
+        
+        if displaying_terminal_message:
+                user_input[0] = None
+                print ("User message overriden successfully, Now calling wordpress in 2 seconds")
+                time.sleep(2)
 
 
 
